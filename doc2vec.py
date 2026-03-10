1d0af30a3ee0c0d569d5dcddb306fef42d5d7193
harsh-marar <harsh.marar@gmail.com>
2026-03-09 23:37:42 -0700

    Final run with image and enrich flags on.

--- doc2vec.py
+++ doc2vec.py
@@ -277,7 +277,7 @@ async def fetch_articles_async(urls):
 
 
 def get_db_conn(host, user, password, database="lab8"):
-    conn = psycopg2.connect(host=host, port=5432, database=database, user=user, password=password)
+    conn = psycopg2.connect(host=host, port=5433, database=database, user=user, password=password)
     conn.autocommit = True
     logging.info(f"Connected to '{database}' on {host}")
     return conn
